50c21da08bf8af3c1c75091c4bc4da7b8248ec96
matimoreira <58670479+matimoreira@users.noreply.github.com>
2020-08-21 23:29:53 -0300

    Agregue el proyecto whishlist

---
+++ angular-whishlist/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DestinoViajeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
