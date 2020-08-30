fd52f032f09051f6f311f83b43a657ed8cca8851
matimoreira <58670479+matimoreira@users.noreply.github.com>
2020-08-30 17:53:26 -0300

    Agregue estilos hechos por el profesor del curso.

--- angular-whishlist/src/app/lista-destinos/lista-destinos.component.ts
+++ angular-whishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { DestinoViaje } from './../models/destino-viaje.models';
 
 @Component({
   selector: 'app-lista-destinos',
@@ -6,12 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-destinos.component.css']
 })
 export class ListaDestinosComponent implements OnInit {
-	destinos: string[];
-  constructor() { 
-		this.destinos = ['Barcelona', 'Buenos Aires', 'Lima', 'Barranquilla'];
-  }
+	destinos: DestinoViaje[];
+	constructor() {
 
-  ngOnInit(): void {
-  }
+		this.destinos = [];
+	}
+
+  	agregar(_nombre: string, _url: string): boolean	{
+
+		console.log(new DestinoViaje(_nombre, _url));  	
+		this.destinos.push(new DestinoViaje(_nombre, _url));
+		// console.log(this.destinos[0]);
+		return false;
+  	}
+
+  	ngOnInit(): void {
+
+  	}
 
 }
---
+++ angular-whishlist/src/app/models/destino-viaje.models.ts
@@ -0,0 +1,9 @@
+export class DestinoViaje {
+	nombre: string;
+	imageUrl: string;
+
+	constructor(n: string, i:string) {
+		this.nombre = n;
+		this.imageUrl = i;
+	}
+}
